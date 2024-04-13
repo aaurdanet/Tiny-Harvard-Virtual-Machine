c432929cc39414ba8327236c24015b8806205bce
Antonio Urdaneta <123696971+aaurdanet@users.noreply.github.com>
2024-04-13 10:59:09 -0400

    Update tinyMachine.c

--- tinyMachine.c
+++ tinyMachine.c
@@ -1,11 +1,9 @@
-/* Antonio Urdaneta 
-CGS3269 04/08/2024 */
-
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-// Defining constants opCodes and specifying array sizes for the Instruction Set Architecture cycles
+
+// Defining constants opCodes and specifying array sizes for the Instruction Set Architecture cyclesint PC = 0;
 #define LOAD 1
 #define ADD 2
 #define STORE 3
@@ -15,151 +13,145 @@ CGS3269 04/08/2024 */
 #define END 7
 #define JMP 8
 #define SKIPZ 9
-#define PROGRAM_SIZE 100
-#define DM_ARRAY_SIZE 10//DM stands for Data Memory
+#define MAXSIZE 100
+#define DM_ARRAY_SIZE 10
+
 
 typedef struct {
-    int opCode, address;
+    int opcode, address;
 } Instruction;
 
-//Global variables that represent the registers and components of 
-//Tiny Harvard Architecture ISA
-int PC = 0;
+// Global variables that represent the registers and components of 
+// Tiny Harvard Architecture ISA
 int MAR1 = 0;
 int MAR2 = 0;
+int PC = 0;
 Instruction MDR1;
 int MDR2 = 0;
 int A = 0;
 int DM[DM_ARRAY_SIZE];
-Instruction IM[PROGRAM_SIZE];// IM stands for Instruction Memory (program memory)
+Instruction IM[MAXSIZE];// IM stands for Instruction Memory (program memory)
 Instruction IR;
+
 
 int main(int argc, char** argv)
 {
-// Openning and reading input1.txt file
-    char *name = argv[1];
-    FILE *input_file = fopen(name,"r");
-    int j = 0;
+    // Opening and reading input file
+    char* input_file = argv[1];
+    FILE* source_file = fopen("input_file", "r");
+    int i = 0;
 
-// Check if the input file pointer is NULL,
-// indicating failure to open the file.
-    if(input_file == NULL)
+    // Check if the input file pointer is NULL,
+    // indicating failure to open the file.
+    if (source_file == NULL)
     {
-        printf("No file found\n");
+        printf("Please open file from terminal/command line.\n");
         return 1;
     }
-// If the input file was opened successfully,
-// read data from the file until reaching the end of the file EOF.
+    // If the input file was opened successfully,
+    // read data from the file until reaching the end of the file EOF.
     else
     {
-        while(fscanf(input_file,"%d%d", &IM[j].opCode, &IM[j].address)!= EOF)
+        while (fscanf(source_file, "%d%d", &IM[i].opcode, &IM[i].address) != EOF)
         {
-            j++;
+            i++;
         }
-        fclose(input_file);
+        fclose(source_file);
     }
 
     printf("\nAssembling Program...\n");
     printf("Program Assembled.\n");
-    printf("Run.\n\n");
+    printf("\nRunning\n\n");
 
-    int run = 1;
+    int halt = 1;
 
-    while (run == 1)
-    {
-        
-    // FETCH Instruction
+    while (halt == 1) {
+        // FETCH Instruction
         MAR1 = PC;
-        PC+=2;
-        MDR1 = IM[MAR1];
-        IR = MDR1;
+        IR = IM[MAR1];
 
-        switch(IR.opCode)
+        // Execute instruction based on opcode
+        switch (IR.opcode)
         {
-    // LOAD instruction 
-            case LOAD:
-               printf("Loading value from memory location %d into the accumulator\n", IR.address);
-                MAR2 = IR.address;
-                MDR2 = DM[MAR2];
-                A = MDR2;
-                break;
-
-    // ADD instruction 
-            case ADD:
-                printf("Adding value from memory location %d to the accumulator\n", IR.address);
-                MAR2 = IR.address;
-                MDR2 = DM[MAR2];
-                A = A + MDR2;
-                break;
-
-    // STORE instruction 
-            case STORE:
+            // LOAD instruction
+        case LOAD:
+            printf("Loading value at memory location %d to A:\n", IR.address);
+            MAR2 = IR.address;
+            MDR2 = DM[MAR2];
+            A = MDR2;
+            break;
+            // ADD instruction
+        case ADD:
+            printf("Adding value from memory location %d to the accumulator\n", IR.address);
+            MAR2 = IR.address;
+            MDR2 = DM[MAR2];
+            A += MDR2;
+            break;
+            // STORE instruction
+        case STORE:
             printf("Storing the value of the accumulator to memory location %d\n", IR.address);
-                MAR2 = IR.address;
-                MDR2 = A;
-                DM[MAR2] = MDR2;
-                break;
-
-    // SUB instruction 
-            case SUB:
+            MAR2 = IR.address;
+            MDR2 = A;
+            DM[MAR2] = MDR2;
+            break;
+            // SUB instruction
+        case SUB:
             printf("Subtracting the value at memory location %d from the accumulator\n", IR.address);
-                MAR2 = IR.address;
-                MDR2 = DM[MAR2];
-                A = A - MDR2;
-                break;
-
-    // INPUT instruction 
-            case IN:
-                printf("Input device number:\n");
-                printf("%d\n", IR.address);
-                A = IR.address;
-                break;
-
-    // OUT instruction 
-            case OUT:
-                printf("Outputting accumulator to screen\n");
-                printf("%d\n", A);
-                break;
-
-    // END instruction 
-            case END:
-                printf("End of execution\n");
-                run = 0;
-                break;
-
-    // JUMP instruction 
-            case JMP:
-                printf("Jumping to memory address %d\n", IR.address);
-                PC = IR.address;
-                break;
-
-    // SKIPZ instruction
-            case SKIPZ:
+            MAR2 = IR.address;
+            MDR2 = DM[MAR2];
+            A -= MDR2;
+            break;
+            // IN instruction
+        case IN:
+            printf("Input device number:\n");
+            printf("%d\n", IR.address);
+            A = IR.address;
+            break;
+            // OUT instruction
+        case OUT:
+            printf("Outputting accumulator to screen\n");
+            printf("%d\n", A);
+            break;
+            // END instruction
+        case END:
+            printf("End of execution\n");
+            halt = 0;
+            continue;
+            // JUMP instruction
+        case JMP:
+            printf("Jumping to memory address %d\n", IR.address);
+            PC = IR.address - 2;
+            break;
+            // SKIPZ instruction
+        case SKIPZ:
+            printf("Checking if the accumulator equals 0\n");
+            if (A == 0)
+            {
                 printf("Checking if the accumulator equals 0\n");
-                if (A == 0)
-                {
-                    printf("Checking if the accumulator equals 0\n");
-                    PC += 2;
-                }
-                else
-                    printf("False; executing the next instruction\n");
-                break;
-
-    //No execution for default case
-            default:
-                printf("Invalid instruction\n");
+                PC += 2;
+            }
+            else
+            {
+                printf("False; executing the next instruction\n");
+            }
+            break;
+            // No execution for default case
+        default:
+            printf("Invalid instruction\n");
         }
 
-    //Printing the PC, A, and the DM to user after every instruction
+        // Printing the PC, A, and the DM to user after every instruction
         printf("PC = %d | A = %d | DM = [", PC, A);
-        for (int i = 0; i < DM_ARRAY_SIZE; i++)
+        for (int j = 0; j < DM_ARRAY_SIZE; j++)
         {
-            if (i == DM_ARRAY_SIZE - 1)
-                printf("%d]\n\n", DM[i]);
-            else 
-                printf("%d, ", DM[i]);
+            if (j < DM_ARRAY_SIZE - 1)
+                printf("%d, ", DM[j]);
+            else
+                printf("%d]\n\n", DM[j]);
         }
+        PC += 2;
     }
-    printf("Program Complete\n");
+
+    printf("Program Completed\n");
     return 0;
 }
